1e6d86e1f753c5d5a9eae28e121782204845a7ec
Gabriel-SH3 <gabriel.meireles@sh3.com.br>
2024-01-30 09:43:41 -0300

    [Feature] Modelos de User, Ticket, Service, ServiceAreas.
    Criação dos Modelos necessários na aplicação.

---
+++ app/Models/Service.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Service extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'requester_name',
+        'client_id',
+        'service_area',
+        'support_id',
+        'status',
+        'service',
+    ];
+
+    protected $attributes = [
+        'status' => false,
+        'service' => '',
+    ];
+
+    protected $with = ['user'];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class, 'client_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'support_id');
+    }
+}
