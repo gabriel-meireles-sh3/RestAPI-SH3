435bca6e54d93d5141ee0d8be5b1293e0ef4f474
Gabriel-SH3 <gabriel.meireles@sh3.com.br>
2024-01-30 10:14:41 -0300

    [Tests] Adição de funções de testes.
    Testes para FindAllTickets, FindTicketsById, FindTicketsByInvalidId, DeleteTicketById e DeleteTicketByInvalidId.

--- tests/Feature/TicketControllerTest.php
+++ tests/Feature/TicketControllerTest.php
@@ -56,4 +56,62 @@ class TicketControllerTest extends TestCase
         ]);
         $this->assertDatabaseHas('tickets', $updatedTicketData);
     }
+
+    public function testFindAllTickets()
+    {
+        $user = User::factory()->create(['role' => User::ROLE_ADMIN]);
+        $tickets = Ticket::factory(3)->create();
+        $response = $this->actingAs($user)->getJson('/api/getTickets');
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            '*' => ['id', 'name', 'client', 'occupation_area', 'created_at', 'updated_at'],
+        ]);
+    }
+
+    public function testFindTicketById()
+    {
+        $user = User::factory()->create(['role' => User::ROLE_ADMIN]);
+        $ticket = Ticket::factory()->create();
+
+        $response = $this->actingAs($user)->get('/api/getTicket?id=' . $ticket->id);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'id' => $ticket->id,
+            'name' => $ticket->name,
+            'client' => $ticket->client,
+            'occupation_area' => $ticket->occupation_area,
+        ]);
+        $response->assertJsonStructure([
+            'id', 'name', 'client', 'occupation_area', 'created_at', 'updated_at',
+        ]);
+    }
+
+    public function testFindTicketByInvalidId()
+    {
+        $user = User::factory()->create(['role' => User::ROLE_ADMIN]);
+
+        $response = $this->actingAs($user)->get('/api/getTicket?id=999');;
+        $response->assertStatus(404);
+        $response->assertJson(['message' => 'Ticket not found']);
+    }
+
+    public function testDeleteTicketById()
+    {
+        $user = User::factory()->create(['role' => User::ROLE_ADMIN]);
+        $ticket = Ticket::factory()->create();
+
+        $response = $this->actingAs($user)->deleteJson('/api/deleteTicket', ['id' => $ticket->id]);
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('tickets', ['id' => $ticket->id]);
+    }
+
+    public function testDeleteTicketByInvalidId()
+    {
+        $user = User::factory()->create(['role' => User::ROLE_ADMIN]);
+        
+        $response = $this->actingAs($user)->deleteJson('/api/deleteTicket', ['id' => 999]);
+        $response->assertStatus(404);
+        $response->assertJson(['message' => 'Ticket not found']);
+    }
 }
