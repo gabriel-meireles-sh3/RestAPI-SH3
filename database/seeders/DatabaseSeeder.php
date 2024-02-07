46b0e879af48aec5bf8ea4758ec1e921a0fb1743
Gabriel-SH3 <gabriel.meireles@sh3.com.br>
2024-02-07 11:35:25 -0300

    [Feature] DataBase seeder.
    Implementação do dataBase seeder para popular o banco de dados.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Service;
+use App\Models\ServiceAreas;
+use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => "teste@gmail.com",
+            'senha' => "123456",
+        ]);
+        User::factory(10)->create();
+        $user = User::factory()->create(['role' => User::ROLE_SUPPORT]);
+        $serviceArea = ServiceAreas::factory()->create(['user_id' => $user->id]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ServiceAreas::factory(10)->create();
+        Ticket::factory(10)->create();
+
+        Service::factory(10)->create();
+        Service::factory()->create(['support_id' => NULL]);
+        Service::factory()->create(['status' => true]);
+        Service::factory()->create(['service_area' => $serviceArea->service_area]);
     }
 }
